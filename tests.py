22cc3b9e324b09decce0a605daec237e586bcc04
Wok <woctezuma@users.noreply.github.com>
2019-07-28 11:20:35 +0200

    Unit test: ensure that results are identical if the top 50 games or the top 51 games are excluded.
    This shows that the recommender likely does not take into account the
    exclusion of games outside of the top 50 games, and there is no point of
    trying to play with this setting.

--- tests.py
+++ tests.py
@@ -1,9 +1,40 @@
 import unittest
 
-class TestTODOMethods(unittest.TestCase):
+import download_results
+import file_utils
 
-    def test_main(self):
-        self.assertTrue(True)
+
+def get_app_id_for_sleeping_dogs():
+    # In July 2019, Sleeping Dogs is my 51st most played game. So it does not appear on the web page of the recommender.
+    app_id_for_sleeping_dogs = 202170
+
+    return app_id_for_sleeping_dogs
+
+
+class TestDownloadResultsMethods(unittest.TestCase):
+
+    def test_exclusion_of_a_game_outside_of_the_top_50(self):
+
+        ignored_app_ids = file_utils.load_input_app_ids()
+
+        result_A = download_results.download_recommender_results(ignored_app_ids=ignored_app_ids)
+
+        ignored_app_ids = file_utils.load_input_app_ids()
+        if get_app_id_for_sleeping_dogs() in ignored_app_ids:
+            raise AssertionError()
+        ignored_app_ids.append(get_app_id_for_sleeping_dogs())
+
+        result_B = download_results.download_recommender_results(ignored_app_ids=ignored_app_ids)
+
+        for (a, b) in zip(result_A['recommendations'], result_B['recommendations']):
+            if a['app_ids'] != b['app_ids']:
+                print('Results differ: exclusion of the top 50 games vs. exclusion of the top 51 games.')
+                print(a['algorithm_options'])
+                print(b['algorithm_options'])
+                print()
+
+        return
+
 
 if __name__ == '__main__':
     unittest.main()
