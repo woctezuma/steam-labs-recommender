ce18b74a582310e3259082856c4f2bae0b7604f2
Wok <woctezuma@users.noreply.github.com>
2019-07-28 14:30:53 +0200

    Move to experiments.py the exclusion test of a game out of the top 50

---
+++ experiments.py
@@ -0,0 +1,41 @@
+from download_results import download_recommender_results
+from file_utils import load_input_app_ids
+
+
+def get_app_id_for_sleeping_dogs():
+    # In July 2019, Sleeping Dogs is my 51st most played game. So it does not appear on the web page of the recommender.
+    app_id_for_sleeping_dogs = 202170
+
+    return app_id_for_sleeping_dogs
+
+
+def test_exclusion_of_a_game_outside_of_the_top_50():
+    ignored_app_ids = load_input_app_ids()
+
+    result_A = download_recommender_results(ignored_app_ids=ignored_app_ids)
+
+    ignored_app_ids = load_input_app_ids()
+    if get_app_id_for_sleeping_dogs() in ignored_app_ids:
+        raise AssertionError()
+    ignored_app_ids.append(get_app_id_for_sleeping_dogs())
+
+    result_B = download_recommender_results(ignored_app_ids=ignored_app_ids)
+
+    for (a, b) in zip(result_A['recommendations'], result_B['recommendations']):
+        if a['app_ids'] != b['app_ids']:
+            print('Results differ: exclusion of the top 50 games vs. exclusion of the top 51 games.')
+            print(a['algorithm_options'])
+            print(b['algorithm_options'])
+            print()
+
+    return
+
+
+def main():
+    test_exclusion_of_a_game_outside_of_the_top_50()
+
+    return
+
+
+if __name__ == '__main__':
+    main()
